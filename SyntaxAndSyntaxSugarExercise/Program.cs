3f17ea7b84fa9a2985dda471066c918120c29545
Andrae Vang <andrae.vang@gmail.com>
2024-06-04 14:31:36 -0700

    completed task

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //inferred typing, String Interpolation, and Ternary Operator
+
+            var answer = 4;
+            var response = (answer < 9)?$"{answer} is less than mine" : $"{answer} is greater than mine";
+            Console.WriteLine(response);
+
         }
     }
 }
